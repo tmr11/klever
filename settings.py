b26b416ea240768d82a87485985fbf79051fa9d0
tmr11 <59247534+tmr11@users.noreply.github.com>
2024-09-19 23:39:46 +0700

    Create new project

---
+++ settings.py
@@ -0,0 +1,15 @@
+TOKENS_TO = [
+    '0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2',
+    '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+]
+
+TOKEN_FROM = '0x4200000000000000000000000000000000000006' # token address from (eth from base)
+
+CHAIN_ID = 8453 # Network id  BASE
+
+MIN_AMOUNT = 0.00001  # Minimal amount to swap
+MAX_AMOUNT = 0.0001   # Maximal amount to swap
+
+MAX_GAS_PRICE = 50  # maximal gas price
+MAX_GAS_LIMIT = 200000 # maximal gas limit
+
